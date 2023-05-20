ae0c27001d3e7f9824219d0e8d714ac84e5d2736
molee1354 <molee1354@gmail.com>
2023-05-20 14:05:23 +0900

    added shell

---
+++ shell/alias.sh
@@ -0,0 +1,12 @@
+# alias definitions
+
+alias la="ls -a"
+alias ll="ls -l"
+
+alias vim="/usr/bin/nvim"
+alias clip="xclip -sel clip"
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+
+alias open="xdg-open"
+alias train="sl"
---
+++ shell/main.sh
@@ -0,0 +1,4 @@
+# sourcing the files
+
+source ~/.config/shell/alias.sh
+source ~/.config/shell/functions.sh
