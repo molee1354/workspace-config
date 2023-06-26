5b465e124c3d76765d4fbcfe23d290aa27f5b573
molee1354 <molee1354@gmail.com>
2023-06-26 13:52:06 +0900

    fix

--- shell/alias.sh
+++ shell/alias.sh
@@ -3,7 +3,7 @@
 alias la="ls -a"
 alias ll="ls -l"
 
-alias vim="/usr/bin/nvim"
+alias vim="/opt/nvim-linux64/bin/nvim"
 alias clip="xclip -sel clip"
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
